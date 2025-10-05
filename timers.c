ca002792424659a687b6ea5f998a3dfaf2c81fa0
deeksharamesh1 <115535364+deeksharamesh1@users.noreply.github.com>
2025-10-05 19:45:59 +0530

    Update timers.c

--- timers.c
+++ timers.c
@@ -8,9 +8,9 @@ void init_timer2(void)
     TOUTPS1=0;
     TOUTPS0=0;
     
-    /* Selecting the scale as 1:1 */
-    T2CKPS0 = 0;
-    T2CKPS1 = 0;
+    /* Selecting the scale as 1:16 */
+    T2CKPS0 = 1;
+    T2CKPS1 = 1;
     
 
     /* Loading the Pre Load register with 250 */
@@ -23,3 +23,4 @@ void init_timer2(void)
     /* Switching on the Timer2 *///offing it  ON WHEN REQ!!
     TMR2ON = 0;
 }
+
